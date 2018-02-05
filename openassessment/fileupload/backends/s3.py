cd20d83271d5d6604f39110a51f388c36940676e
Marko D. Topić <mdtopic@extensionengine.com>
2018-02-05 14:20:09 +0100

    Set cors configuration

--- openassessment/fileupload/backends/s3.py
+++ openassessment/fileupload/backends/s3.py
@@ -12,7 +12,16 @@ class Backend(BaseBackend):
     def get_upload_url(self, key, content_type):
         bucket_name, key_name = self._retrieve_parameters(key)
         try:
+            logger.info(bucket_name)
+            logger.info(key_name)
+            logger.info('Connecting to S3')
             conn = _connect_to_s3()
+            logger.info('Connected to S3')
+            logger.info('CORS configuration')
+            bucket = conn.lookup(bucket_name)
+            logger.info(bucket.get_cors())
+            _set_cors_configuration(conn, bucket_name)
+
             upload_url = conn.generate_url(
                 expires_in=self.UPLOAD_URL_TIMEOUT,
                 method='PUT',
@@ -51,6 +60,19 @@ class Backend(BaseBackend):
             return True
         else:
             return False
+
+
+def _set_cors_configuration(conn, bucket_name):
+    cors_cfg = boto.s3.cors.CORSConfiguration()
+    cors_cfg.add_rule(['PUT', 'POST', 'DELETE'], 'https://{}'.format(settings.LMS_BASE),
+                      allowed_header='*',
+                      max_age_seconds=3000,
+                      expose_header='x-amz-server-side-encryption')
+    cors_cfg.add_rule('GET', '*')
+    bucket = conn.lookup(bucket_name)
+    bucket.set_cors(cors_cfg)
+    logger.info('New CORS configuration')
+    logger.info(bucket.get_cors())
 
 
 def _connect_to_s3():
